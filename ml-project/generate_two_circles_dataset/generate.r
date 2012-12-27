790de8a7b7d3abe359fac250dfd3a14988582524
traims <posines@gmail.com>
2012-12-27 20:13:24 +0100

    A sample script to produce two concentric circles (r=5 and r=10)

--- ml-project/generate_two_circles_dataset/generate.r
+++ ml-project/generate_two_circles_dataset/generate.r
@@ -3,7 +3,7 @@
 # r - radius
 
 # let the center be (0,0): x^2 + y^2 = r^2
-# if we know x and r:   y = sqrt(r^2 - x^2)
+# if we know x and r:   y = +/- sqrt(r^2 - x^2)
 # =>
 # generate x coords and then calculate y
 
@@ -13,3 +13,17 @@ set.seed(0)
 x <- sample(-r:r, 100, replace=TRUE)
 y <- sqrt(r^2 - x^2) * sample(c(-1,1), 100, replace=TRUE) # so we can have negative y's as well
 plot(x, y)
+
+r.second <- 10
+set.seed(0)
+x.second <- sample(-r.second:r.second, 100, replace=TRUE)
+y.second <- sqrt(r.second^2 - x.second^2) * sample(c(-1,1), 100, replace=TRUE) # so we can have negative y's as well
+lines(x.second, y.second)
+
+# create a plot
+plot(-15:15, -15:15, type = "n", 
+     main = "Points on two concentric circles",
+     xlab = "Values of x", ylab = "Values of y")
+points(x,y, pch = 5, col = "tomato") # 5 for diamonds
+points(x.second, y.second, pch = 19, col = "light blue")  # 19 for solid circles
+
