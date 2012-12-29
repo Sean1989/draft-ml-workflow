5c46ec8fd78d916d025f84beaa4dab885ba065a4
traims <posines@gmail.com>
2012-12-29 14:54:10 +0100

    Updated the script to generate the dataset; uploaded the output

--- generate_two_circles_dataset/generate.r
+++ generate_two_circles_dataset/generate.r
@@ -1,27 +1,10 @@
-# Circle equation: (x − xc)^2 + (y − yc)^2 = r^2
-# (xc, yc) - center of the circle
-# r - radius
-
-# let the center be (0,0): x^2 + y^2 = r^2
-# if we know x and r:   y = +/- sqrt(r^2 - x^2)
-# =>
-# generate x coords and then calculate y
-
-new.points <- function(class = 0, radius = 5, distortion = 2, nr.of.samples = 100){
-  # Returns a dataframe with points roughly located on a circle
-  # Output:
-  #    - dataframe:
-  #         x | y | class
-  #
-  #      where (x, y) are coordinates of the points, all of the same pre-set "class".
-  #
+# Returns a dataframe with points roughly located on a circle. 
+new.points <- function(class = 0, radius = 5, distortion = 2, nr.of.samples = 100){      
   set.seed(0)
   x <- sample(-radius:radius, nr.of.samples, replace=TRUE)
-  y <- sqrt(radius^2 - x^2) * sample(c(-1,1), nr.of.samples, replace=TRUE) 
-  # introduce distortion: our points should not be exactly on the circle
+  y <- sqrt(radius^2 - x^2) * sample(c(-1,1), nr.of.samples, replace=TRUE)   
   x <- x + sample(-distortion:distortion, nr.of.samples, replace=TRUE)
-  y <- y + sample(-distortion:distortion, nr.of.samples, replace=TRUE)
-  # create a dataframe
+  y <- y + sample(-distortion:distortion, nr.of.samples, replace=TRUE)  
   df <- data.frame(x = x, y = y, class = class)
   return(df)
 }
@@ -30,6 +13,7 @@ new.points <- function(class = 0, radius = 5, distortion = 2, nr.of.samples = 10
 ## Working with data
 ##
 
+# generate points of two different classes
 df.first  <- new.points(class = 0, radius = 5, nr.of.samples = 100)
 df.second <- new.points(class = 1, radius = 15, nr.of.samples = 120)
 
@@ -42,7 +26,7 @@ points(df.first$x,  df.first$y,  pch = 5,  col = "tomato") # 5 for diamonds
 points(df.second$x, df.second$y, pch = 19, col = "light blue")  # 19 for solid circles
 
 
-# append the two datasets
+# append the two dataframes
 dataset <- rbind(df.first, df.second)
 # shuffle the data
 dataset <- dataset[sample(nrow(dataset)),]
