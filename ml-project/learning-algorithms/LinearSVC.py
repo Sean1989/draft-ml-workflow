87734a83878cbd4f78f642e6eb4254ff95595d56
traims <posines@gmail.com>
2013-01-17 22:08:11 +0100

    Fit a linear SVC; lots of things left to do

--- ml-project/learning-algorithms/LinearSVC.py
+++ ml-project/learning-algorithms/LinearSVC.py
@@ -1,15 +1,25 @@
 import pandas
 from sklearn.svm import LinearSVC
 
+# Read the data
+
 data = pandas.read_csv('../data/accurate-dataset.csv')
 n_samples = len(data)
 
-train = data[:132]
-test = data[132:]
+# Split it into train and test sets
+
+boundary = int(n_samples * 0.6)
+train = data[:boundary]
+test = data[boundary:]
+del test['result']
+
+# Fit a Linear Support Vector Classification model
 
 clf = LinearSVC()
 X = train.ix[:, :'y']
 y =  train.ix[:, 'result':]
 clf = clf.fit(X, y)
+
+
 
 
