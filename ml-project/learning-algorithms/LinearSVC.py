76135fd13805e05261715dd6d160135c9181a014
traims <posines@gmail.com>
2013-01-17 22:59:55 +0100

    Evaluated the prediction produced by a plain LinearSVC model

--- ml-project/learning-algorithms/LinearSVC.py
+++ ml-project/learning-algorithms/LinearSVC.py
@@ -1,5 +1,6 @@
 import pandas
 from sklearn.svm import LinearSVC
+from sklearn import metrics
 
 # Read the data
 
@@ -9,17 +10,25 @@ n_samples = len(data)
 # Split it into train and test sets
 
 boundary = int(n_samples * 0.6)
-train = data[:boundary]
-test = data[boundary:]
-del test['result']
+train, test = data[:boundary], data[boundary:]
+X_train, X_test = train.ix[:, :'y'], test.ix[:, :'y']
+y_train, y_test = train.ix[:, 'result':], test.ix[:, 'result':]
 
 # Fit a Linear Support Vector Classification model
 
 clf = LinearSVC()
-X = train.ix[:, :'y']
-y =  train.ix[:, 'result':]
-clf = clf.fit(X, y)
+clf = clf.fit(X_train, y_train)
 
+# Make a prediction
 
+y_pred = clf.predict(X_test)
 
+# Simplify y_test to compare it with y_pred
 
+y_test = [int(i) for i in y_test.values]
+
+# Evaluate the prediction
+
+metrics.precision_score(y_test, y_pred)  # 0.50909090909090904
+metrics.recall_score(y_test, y_pred)     # 0.56000000000000005
+metrics.f1_score(y_test, y_pred)         # 0.53333333333333333
