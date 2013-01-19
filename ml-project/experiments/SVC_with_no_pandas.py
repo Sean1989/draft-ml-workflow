Fixed feature scaling; skipped the first line of csv to avoid NaNs

ml-project/experiments/SVC_with_no_pandas.py
@@ -6,8 +6,12 @@ from sklearn import preprocessing
 
 # Read the data
 
-data = genfromtxt('../data/float_features_dataset.csv', delimiter=',')
+data = genfromtxt('../data/float_features_dataset.csv', delimiter=',', skiprows=1)
 n_samples = len(data)
+
+# Scale features
+
+data[:, 0:-1] = preprocessing.scale(data[:,0:-1])
 
 # Split it into train and test sets
 
@@ -15,10 +19,6 @@ boundary = int(n_samples * 0.6)
 train, test = data[:boundary], data[boundary:]
 X_train, X_test = np.delete(train,-1,1), np.delete(test,-1,1)
 y_train, y_test = train[:,-1], test[:,-1]
-
-# Scale features
-
-X_train, X_test = preprocessing.scale(X_train), preprocessing.scale(X_test)
 
 # Apply a learning algorithm
 
@@ -38,12 +38,13 @@ metrics.f1_score(y_test, y_pred)
 
 # Experiment results:
 
-#In [472]: metrics.precision_score(y_test, y_pred)  
-#Out[472]: 0.5730337078651685
+#In [86]: metrics.precision_score(y_test, y_pred)  
+#Out[86]: 1.0
 
-#In [473]: metrics.recall_score(y_test, y_pred)     
-#Out[473]: 1.0
+#In [87]: metrics.recall_score(y_test, y_pred)     
+#Out[87]: 1.0
 
-#In [474]: metrics.f1_score(y_test, y_pred)         
-#Out[474]: 0.72857142857142854
+#In [88]: metrics.f1_score(y_test, y_pred)         
+#Out[88]: 1.0
+
 