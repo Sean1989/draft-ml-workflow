9da8c8c9cbfb731acea56d31f923a07f258eb64a
traims <posines@gmail.com>
2013-01-29 22:55:11 +0100

    PEP-8 and formatting fixes.

--- python/get_data.py
+++ python/get_data.py
@@ -1,8 +1,14 @@
-## Procedures for reading datasets of different types 
-## * Function `numbers` works for comma-separated files with real or integer numbers 
+## Procedures for reading datasets of different types
+## ____________________________________________________
+##
+## * Function `numbers` works for comma-separated files
+##   with real or integer numbers.
+##
 ## * Convert categorical or text features here.
 
+
 from numpy import genfromtxt
+
 
 def numbers(data_path):
     raw_data = genfromtxt(data_path, delimiter=',')
