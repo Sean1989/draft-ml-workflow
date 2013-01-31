9050d9f0d098a087b2d889964210527dce6dd9eb
traims <posines@gmail.com>
2013-01-31 23:45:04 +0100

    Additional metric: mean accuracy from sklearn

--- python/run.py
+++ python/run.py
@@ -38,10 +38,12 @@ def run(data_path, read_as, algorithm):
     results['Precision'] = float(metrics.precision_score(y_test, y_pred))
     results['Recall'] = float(metrics.recall_score(y_test, y_pred))
     results['F1 score'] = float(metrics.f1_score(y_test, y_pred))
+    results['Mean accuracy'] = clf.score(X_test, y_test)
 
     print "Precision: \t", results['Precision']
     print "Recall: \t", results['Recall']
     print "F1 score: \t", results['F1 score']
+    print "Mean accuracy: \t", results['Mean accuracy']
 
     return results
 
