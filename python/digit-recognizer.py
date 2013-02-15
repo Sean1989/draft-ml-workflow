52486d12a2f61008efdee53999e1fbf9b69fdf80
traims <posines@gmail.com>
2013-02-15 13:02:08 +0100

    Digit recognizer: accuracy 0.805 for n_train=600 and n_test=400

--- python/digit-recognizer.py
+++ python/digit-recognizer.py
@@ -1,20 +1,32 @@
+import time
+import random
+from numpy import arange
 from numpy import genfromtxt
-from numpy.random import shuffle
-from sklearn.cross_validation import train_test_split
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import LinearSVC
 from sklearn import metrics
 
 
-# Read data
-data_path = '../data/digit-recognizer/train_debug.csv'
-data = genfromtxt(data_path, skip_header=1, delimiter=',')
+# Remember starting time
+start_time = time.time()
 
-# Split it into train and test sets
-shuffle(data)
-features, target = data[:, 1:], data[:, 1]
-X_train, X_test, y_train, y_test = train_test_split(
-    features, target, test_size=0.33, random_state=17)
+# Read data
+print "Reading the data..."
+data_path = '../data/digit-recognizer/train.csv'
+raw_data = genfromtxt(data_path, skip_header=1, delimiter=',')
+
+# Trunk the data
+n_train = 600
+n_test = 400
+
+# Define training and testing sets
+print "Splitting into a training and a testing set..."
+indices = arange(len(raw_data))
+random.seed(0)
+train_idx = random.sample(indices, n_train)
+test_idx = random.sample(indices, n_test)
+X_train, y_train = raw_data[train_idx, 1:], raw_data[train_idx, 0]
+X_test, y_test = raw_data[test_idx, 1:], raw_data[test_idx, 0]
 
 # Apply a learning algorithm
 print "Applying a learning algorithm..."
@@ -33,3 +45,6 @@ print "Recall: \t", metrics.recall_score(y_test, y_pred)
 print "F1 score: \t", metrics.f1_score(y_test, y_pred)
 print "Mean accuracy: \t", clf.score(X_test, y_test)
 
+# Calculate overall time
+end_time = time.time()
+print "Overall running time:", end_time - start_time
