9ec7f5c14cb44c49b0756d479e96422931bbfe9f
traims <posines@gmail.com>
2013-02-24 20:17:11 +0100

    Iris: 200-fold, 75 training samples, 75 test samples. Avg score: 0.962066666667

--- python/iris.py
+++ python/iris.py
@@ -22,7 +22,7 @@ n_folds = 200
 for i in range(n_folds):
 
     X_train, X_test, y_train, y_test = train_test_split(
-        iris.data, iris.target, test_size=0.4, random_state=i)
+        iris.data, iris.target, test_size=0.5, random_state=i)
 
     clf = SVC(kernel='rbf')
     clf.fit(X_train, y_train)
