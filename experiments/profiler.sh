58cd2f7170fe32c1cdb0f2bf902aa7b1dc26997a
traims <posines@gmail.com>
2013-02-28 23:50:42 +0100

    Removed old profiling script and results

--- experiments/profiler.sh
+++
@@ -1,7 +0,0 @@
-# Running SVC on MNIST with plop profiler
-#
-# plop, Python low-overhead profiler
-# https://github.com/bdarnell/plop
-#
-python -m plop.collector mnist_run.py
-python -m plop.viewer --datadir=/tmp
